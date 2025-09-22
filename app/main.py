c26017fb99c2344ef4bfeb0e1651436d5e39c7a6
21javi21 <21javi21@gmail.com>
2025-09-22 15:48:25 +0000

    update app with login page

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app import auth_ldap   # make sure app/auth_ldap.py exists
 
 app = FastAPI()
+
+app.include_router(auth_ldap.router)
 
 @app.get("/")
 def read_root():
